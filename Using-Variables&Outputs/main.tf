1cf001e9ed1c6ac45121e65242ec996a592ab980
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-01-11 03:11:47 +0530

    main.tf update for bucket specs

--- Using-Variables&Outputs/main.tf
+++ Using-Variables&Outputs/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
-    key            = "04-variables-and-outputs/examples/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "shreyash-terraform-state-1234"
+    key            = "learning-terraform/webapp/dev/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 locals {
@@ -38,8 +38,8 @@ resource "aws_db_instance" "db_instance" {
   storage_type        = "gp2"
   engine              = "postgres"
   engine_version      = "12"
-  instance_class      = "db.t2.micro"
-  name                = "mydb"
+  instance_class      = "db.t3.micro"
+  db_name                = "mydb"
   username            = var.db_user
   password            = var.db_pass
   skip_final_snapshot = true
